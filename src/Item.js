d5035d8280235bb146f3d6b57e7829e416017af4
MatthewCrust <115628064+MatthewCrust@users.noreply.github.com>
2024-01-09 19:25:52 +0100

    Add files via upload

--- src/Item.js
+++ src/Item.js
@@ -1,17 +1,27 @@
 import './Item.css';
+import React, { useState } from 'react';
+function Item({ id, onRemove }) {
 
-function Item(){
+  const [backgroundColor, setBackgroundColor] = useState('initialColor');
+
+  const handleRemove = () => {
+    onRemove(id);
+  };
+
+  const markAsDone = () =>{
+    setBackgroundColor('#33cc33');
+  }
 
   return (
-    <div className="item-container">
+    <div style={{ backgroundColor: backgroundColor }} className="item-container">
       <input className='name-input' type="text"placeholder="Název"/>
 
       <input className='quantity-input' type="number" placeholder="Počet"/>
 
       <span>ks</span>
 
-      <button type="button" class="btn btn-success">Hotovo</button>
-      <button type="button" class="btn btn-danger">Odebrat</button>
+      <button onClick={markAsDone} type="button" class="btn btn-success">Hotovo</button>
+      <button onClick={handleRemove} type="button" class="btn btn-danger">Odebrat</button>
     </div>
   );
 };
--- src/SeznamPage.js
+++ src/SeznamPage.js
@@ -1,8 +1,61 @@
+import React, { useState } from 'react';
 import './SeznamPage.css'
 import Navbar from './Navbar';
 import Item from './Item';
+import Modal from 'react-modal';
 
 function List() {
+
+  const [items, setItems] = useState([]);
+
+  const [isArchived, setArchive] = useState(false);
+
+  const [isDone, setDone] = useState(false);
+
+
+  const addItem = () => {
+    const newItem = { id: Date.now() };
+    setItems(prevItems => [...prevItems, newItem]);
+  };
+
+  const removeItem = (itemId) => {
+    setItems(prevItems => prevItems.filter(item => item.id !== itemId));
+  };
+
+  const removeAll = () => {
+    setItems([]);
+  };
+
+  const archive = () => {
+    setArchive(true)
+    alert("Seznam byl archivován")
+  };
+
+  const done = () => {
+    setDone(true)
+    alert("Seznam byl označen jako hotový")
+  };
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const [users, setUsers] = useState(['User1', 'User2', 'User3']);
+  const [newUserName, setNewUserName] = useState('');
+
+  const addUser = () => {
+    setUsers(prevUsers => [...prevUsers, newUserName]);
+    setNewUserName('');
+    closeModal();
+  };
+
+
     return (
       <div>
         <Navbar />
@@ -13,22 +66,38 @@ function List() {
           </div>
 
           <div className='list-items'>
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {items.map(item => (
+              <Item key={item.id} id={item.id} onRemove={removeItem} />
+            ))}
           </div>
 
-          <button type="button" class="btn btn-primary">Přidat položku</button>
+
+          <button onClick={addItem} type="button" class="btn btn-primary">Přidat položku</button>
           
           <div className='seznam-button-box'>
-            <button type="button" class="btn btn-primary">Přidat uživatele</button>
-            <button type="button" class="btn btn-success">Označit jako hotové</button>
-            <button type="button" class="btn btn-warning">Archivovat</button>
-            <button type="button" class="btn btn-danger">Smazat položky</button>
+            <button onClick={openModal} type="button" class="btn btn-primary">Přidat uživatele</button>
+            <button onClick={done} type="button" class="btn btn-success">Označit jako hotové</button>
+            <button onClick={archive} type="button" class="btn btn-warning">Archivovat</button>
+            <button onClick={removeAll} type="button" class="btn btn-danger">Smazat položky</button>
           </div>
 
         </div>
+
+        <Modal className="modal-custom-style" isOpen={isModalOpen} onRequestClose={closeModal} contentLabel="Přidat uživatele">
+          <h2 id='modal-title'>Přidat uživatele</h2>
+          <div className='modal-box'>
+            <input placeholder='Jméno uživatele' type="text" value={newUserName} onChange={(e) => setNewUserName(e.target.value)}/>
+            <button class="btn btn-primary" onClick={addUser}>Přidat uživatele</button>
+            <button class="btn btn-danger" onClick={closeModal}>Zavřít</button>
+
+            <h3>Seznam uživatelů:</h3>
+            <ul>
+              {users.map((user, index) => (
+                <li key={index}>{user}</li>
+              ))}
+            </ul>
+          </div>
+        </Modal>
 
       </div>
 
